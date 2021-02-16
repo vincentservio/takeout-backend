b6150b17aca2071ea7338076f53133b40d26bfaf
vincentservio <vincentservio@gmail.com>
2021-02-16 13:30:54 -0500

    first heroku commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :takeouts
-  root :to => "takeouts#index"
+  root :to => "takeout#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
